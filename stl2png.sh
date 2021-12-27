4e097a8cab869f3d371b10883debdc5961ab4370
Spencer Owen <owenspencer@gmail.com>
2021-12-26 19:49:29 -0700

    stl2png

--- stl2png.sh
+++ stl2png.sh
@@ -1,5 +1,16 @@
+#!/bin/bash
+
+if [ -z "$(docker --version)" ]; then
+	echo "Docker is not installed. Please install docker before running this script."
+	exit 1
+fi
+echo "Updating docker container"
+docker pull spuder/openscad:latest
+
 find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do 
 	echo "Reading $file"
-	#TODO: replace wth spuder/openscad docker container
-	/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD /dev/null -D "import(\"$file\");" -o "$file.png" --imgsize=600,600 --colorscheme "Tomorrow Night" --viewall --autocenter
+	# /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD /dev/null -D "import(\"$file\");" -o "$file.png" --imgsize=600,600 --colorscheme "Tomorrow Night" --viewall --autocenter
+	filename=$(basename "$file")
+	echo "Basename: $filename"
+	docker run --rm -v $(PWD):/data spuder/openscad:latest openscad /dev/null -o "/data/CAD/$filename.png" -D "import(\"/data/CAD/$filename\");" --imgsize=600,600 --camera=0,0,0,45,0,50,25 --autocenter --viewall
 done
