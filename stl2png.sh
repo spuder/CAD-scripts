9f2d74e79022fe97ecb8f811015d07e115f22b2c
Spencer Owen <owenspencer@gmail.com>
2021-12-27 00:10:45 -0700

    Use dark theme

--- stl2png.sh
+++ stl2png.sh
@@ -12,5 +12,5 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
 	# /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD /dev/null -D "import(\"$file\");" -o "$file.png" --imgsize=600,600 --colorscheme "Tomorrow Night" --viewall --autocenter
 	filename=$(basename "$file")
 	echo "Basename: $filename"
-	docker run --rm -v $(PWD):/data spuder/openscad:latest openscad /dev/null -o "/data/CAD/$filename.png" -D "import(\"/data/CAD/$filename\");" --imgsize=600,600 --camera=0,0,0,45,0,50,25 --autocenter --viewall
+	docker run --rm -v $(PWD):/data spuder/openscad:latest openscad /dev/null -o "/data/CAD/$filename.png" -D "import(\"/data/CAD/$filename\");" --imgsize=600,600 --colorscheme "Tomorrow Night" --autocenter --viewall
 done
