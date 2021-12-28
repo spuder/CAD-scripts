c080db717a27bebd9bfd14bf1294b049aedea9b6
Spencer Owen <owenspencer@gmail.com>
2021-12-27 23:32:56 -0700

    move to root

--- yaml2md.rb
+++ yaml2md.rb
@@ -4,8 +4,8 @@ require 'yaml'
 require 'erb' #https://stackoverflow.com/a/25626629/1626687
 include ERB::Util
 
-raise "templates/settings.yaml does not exist, could not load settings" unless File.exists?("templates/settings.yaml")
-settings = YAML.load_file('templates/settings.yaml')
+raise "../settings.yaml does not exist, could not load settings" unless File.exists?(File.join(__dir__,'../settings.yaml'))
+settings = YAML.load_file(File.join(__dir__,'../settings.yaml'))
 
 # Loop through all files ending in .erb in the templates directory
 Dir.glob("templates/*.erb").each do |file|
