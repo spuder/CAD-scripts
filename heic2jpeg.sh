9ffcc9e1539448185cb158d5dacba7c19e71f563
Spencer Owen <owenspencer@gmail.com>
2021-12-27 22:39:18 -0700

    cleanup

--- heic2jpeg.sh
+++ heic2jpeg.sh
@@ -3,12 +3,25 @@
 
 set -eu -o pipefail
 
+#TODO: support .PNG and .JPG
+
+# Convert HEIC to JPEG
 find ~+ -type f -name "*.HEIC" -print0 | while read -d '' -r file; do 
     echo "Converting $file"
     magick mogrify -monitor -format jpeg "$file"
 done
 
+# Copy all .JPEG to 'cropped' folder then resize to 1280x960 for uplading to thingiverse
+find ~+ ! -path "*/cropped/*" -type f -name "*.jpeg" -print0 | while read -d '' -r file; do 
+    dir=$(dirname "$file")
+    base=$(basename "$file" .jpeg)
+    mkdir -p "${dir}/cropped" || true
+    cp "${dir}/${base}.jpeg" "${dir}/cropped/${base}.jpeg"
+    echo "Cropping $dir/cropped/$base.jpeg"
+    mogrify -resize 1280x960 "$dir/cropped/$base.jpeg"
+done
 
+# Cleanup all HEIC files that were converted to JPEG
 find ~+ -type f -name "*.jpeg" -print0 | while read -d '' -r file; do 
     dir=$(dirname "$file")
     base=$(basename "$file" .jpeg)
