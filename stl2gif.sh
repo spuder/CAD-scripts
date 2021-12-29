328f544e741ce53a3bee827b0ecfcef410dbc38f
Spencer Owen <owenspencer@gmail.com>
2021-12-29 14:01:26 -0700

    Center STL

--- stl2gif.sh
+++ stl2gif.sh
@@ -1,12 +1,42 @@
+#!/bin/bash
+
+set -u
+docker pull spuder/stl2origin:latest
+
 find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do 
+
 	echo "Reading $file"
     MYTMPDIR="$(mktemp -d)"
     trap 'rm -rf -- "$MYTMPDIR"' EXIT
 
     echo "Creating temp directory ${MYTMPDIR}"
 
-    #TODO: replace with spuder/openscad docker container
-    /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD /dev/null -D '$vpr = [60, 0, 360 * $t];' -o "${MYTMPDIR}/foo.png"  -D "import(\"$file\");" --imgsize=300,300 --animate 60 --colorscheme "Tomorrow Night" --viewall --autocenter
+    # Detect how offcenter STL file is. Save variable output to foo.sh ($XTRANS, $YTRANS, $ZTRANS)
+    echo "Centering the STL file ${file}"
+    docker run \
+        -e OUTPUT_BASH_FILE=/output/foo.sh \
+        -v $(dirname "$file"):/input \
+        -v $MYTMPDIR:/output \
+        --rm spuder/stl2origin:latest \
+        "/input/$(basename "$file")"
+    cp "${file}" "$MYTMPDIR/foo.stl"
+    source $MYTMPDIR/foo.sh
+    # Create new (temporary) STL that has been centered
+    docker run \
+        -v "$MYTMPDIR:/input" \
+        -v "$MYTMPDIR:/output" \
+        openscad/openscad:2021.01 openscad /dev/null -D "translate([$XTRANS,$YTRANS,$ZTRANS])import(\"/input/foo.stl\");" -o "/output/foo-centered.stl"
+
+    #TODO: replace with openscad/openscad docker container
+    # https://github.com/openscad/openscad/issues/4028
+    /Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD /dev/null \
+        -D '$vpr = [60, 0, 360 * $t];' \
+        -o "${MYTMPDIR}/foo.png"  \
+        -D "import(\"${MYTMPDIR}/foo-centered.stl\");" \
+        --imgsize=300,300 \
+        --animate 60 \
+        --colorscheme "Tomorrow Night" \
+        --viewall --autocenter
 
     #TODO: replace with docker container
     yes | ffmpeg \
