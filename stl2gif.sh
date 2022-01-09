4ea1eb10f612e5b95dc3fa26ee7148db25f1b694
Spencer Owen <spencer.owen@mx.com>
2022-01-09 13:38:58 -0700

    use docker volumes

--- stl2gif.sh
+++ stl2gif.sh
@@ -9,6 +9,12 @@ docker pull spuder/stl2origin:latest
 docker pull linuxserver/ffmpeg:version-4.4-cli
 docker pull openscad/openscad:2021.01
 
+echo "Creating temporary docker volumes stl2gif-input and stl2gif-output"
+docker volume create --name stl2gif-input
+INPUT_ID=$(docker run -d -v stl2gif-input:/input busybox true)
+docker volume create --name stl2gif-output
+OUTPUT_ID=$(docker run -d -v stl2gif-output:/output busybox true)
+
 find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do 
 
     filename=$(basename "$file" ".stl")
@@ -18,28 +24,36 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
     trap 'rm -rf -- "$MYTMPDIR"' EXIT
     echo "Creating temp directory ${MYTMPDIR}"
 
+    echo "Copying ${filename}.stl to stl2gif-input docker volume"
+	docker cp "${dirname}/${filename}.stl" "${INPUT_ID}:/input/${filename}.stl"
+
     # Detect how offcenter STL file is. Save variable output to foo.sh ($XTRANS, $YTRANS, $ZTRANS)
     echo ""
     echo "Detecting ${filename} offset from origin"
     echo "========================================"
+
     docker run \
+        -e OUTPUT_STDOUT=true \
         -e OUTPUT_BASH_FILE=/output/foo.sh \
-        -v $(dirname "$file"):/input \
-        -v $MYTMPDIR:/output \
+        -v stl2gif-input:/input \
+        -v stl2gif-output:/output \
         --rm spuder/stl2origin:latest \
-        "/input/$(basename "$file")"
-    cp "${file}" "$MYTMPDIR/foo.stl"
+        "/input/${filename}.stl"
+
+	docker cp "${OUTPUT_ID}:/output/foo.sh" "${MYTMPDIR}/foo.sh"
+
+    # cp "${file}" "$MYTMPDIR/foo.stl"
     source $MYTMPDIR/foo.sh
-    cat $MYTMPDIR/foo.sh
+    # cat $MYTMPDIR/foo.sh
 
     echo ""
     echo "Duplicating ${filename} and centering object at origin"
     echo "======================================================"
     docker run \
-        -v "$MYTMPDIR:/input" \
-        -v "$MYTMPDIR:/output" \
-        openscad/openscad:2021.01 openscad /dev/null -D "translate([$XTRANS-$XMID,$YTRANS-$YMID,$ZTRANS-$ZMID])import(\"/input/foo.stl\");" -o "/output/foo-centered.stl"
-
+        -v stl2gif-input:/input \
+        -v stl2gif-output:/output \
+        openscad/openscad:2021.01 openscad /dev/null -D "translate([$XTRANS-$XMID,$YTRANS-$YMID,$ZTRANS-$ZMID])import(\"/input/${filename}.stl\");" -o "/output/foo-centered.stl"
+    docker cp "${OUTPUT_ID}:/output/foo-centered.stl" "${MYTMPDIR}/foo-centered.stl"
     # Take temporary (centered) stl and create collection of .png images to be animated
     # TODO: move to docker once opengl issue is solved
     # https://github.com/openscad/openscad/issues/4028
@@ -57,6 +71,13 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
         echo "OpenSCAD is not installed. Please install openscad before running this script."
         exit 1
     fi
+    # Check openscad version is 2020.01 or later
+    openscad_version=$($openscad_path -v 2>&1 | grep -o '\d\d\d\d')
+    if [ "$openscad_version" -lt "2021" ]; then
+        echo "OpenSCAD 2021.01 or later is required to run this script. Please update openscad before running this script."
+        exit 1
+    fi
+
     $openscad_path /dev/null \
         -D '$vpr = [60, 0, 360 * $t];' \
         -o "${MYTMPDIR}/foo.png"  \
@@ -87,13 +108,21 @@ find ~+ -type f -name "*.stl" -print0 | while read -d '' -r file; do
 	# 	-v "${MYTMPDIR}:/output" \
 	# 	openscad/openscad:2021.01 xvfb-run -a openscad "/input/foo.scad" -o "/output/foo.png" --animate 60
     # ls $MYTMPDIR
+    
+    echo ""
+    echo "Copying .png files to docker volume"
+    find ${MYTMPDIR} -type f -name "*.png" -print0 | while read -d '' -r file; do 
+        docker cp "${file}" "${INPUT_ID}:/input/" 
+    done
+
     echo ""
     echo "Converting ${filename} .PNG files into .GIF"
     echo "==========================================="
     docker run --rm \
-        -v "${MYTMPDIR}:/input" \
-        -v "${dirname}:/output" \
+        -v stl2gif-input:/input \
+        -v stl2gif-output:/output \
         linuxserver/ffmpeg:version-4.4-cli -y -framerate 15 -pattern_type glob -i 'input/*.png' -r 25 -vf scale=512:-1 "/output/${filename}.gif";
+    docker cp "${OUTPUT_ID}:/output/${filename}.gif" "${dirname}/${filename}.gif"
     ls "${MYTMPDIR}"
     echo ""
     echo "Cleaning up temp directory ${MYTMPDIR}"
